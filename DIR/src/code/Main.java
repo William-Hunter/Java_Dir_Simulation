e39ddb4294bfcef692b657d6a9e5551e7760828e
william·lone <donwilliamlone@gmail.com>
2015-06-23 22:27:38 +0800

    添加了注释

--- DIR/src/code/Main.java
+++ DIR/src/code/Main.java
@@ -5,19 +5,19 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Dir object=new Dir();
-		while(!object.CMDline()){
+		while(!object.CMDline()){		//输入功能，如果输入非法，就循环执行
 		}
-		object.ShowFileList();
-		System.out.println("over");
+		object.ShowFileList();				//显示制定路径的文件目录
+		System.out.println("执行完毕");				
 	}
 }
 class Dir{
-	String path = null;
+	String path = null;		//存储文件路径的变量
 
 	boolean CMDline(){
 		Scanner in=new Scanner(System.in);
-		System.out.print("输入你的路径：");
-		if(!in.next().equals("dir")){
+		System.out.print("输入路径：");
+		if(!in.next().equals("dir")){		//判断用户输入的第一个字段是不是dir
 			System.out.println("命令不合法");
 			return false;
 		}
@@ -25,7 +25,7 @@ class Dir{
 		in.close();
 		Scanner word=new Scanner(path);
 		if(word.hasNext()){
-			path=word.next();
+			path=word.next();			//获取dir 命令后面的路径名
 		}
 		word.close();
 		return true;
@@ -33,9 +33,9 @@ class Dir{
 	boolean ShowFileList(){
 		File files=new File(path);
 		/////实现循环输出文件目录的第一种方法
-		String[] filelist=files.list();
+		String[] filelist=files.list();				//获取路径文件目录存入一个String 数组
 		for(int i=0;i<filelist.length;i++){
-			System.out.println(filelist[i]);
+			System.out.println(filelist[i]);			//循环打印这个数组里的内容
 		}
 		/*
 		/////第二种
